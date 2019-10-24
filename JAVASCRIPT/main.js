0dba0c15f26bc6898158416fa6a5e277c4baec26
Madhra Palwinder <palwnider.madhra87@email.com>
2019-10-24 13:47:49 +0200

    update notes json

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,20 +1,20 @@
-const person = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    hobbies: ['music', 'movies'],
-    address: {
-        street: '50 main st',
-        city: 'Boston',
-        state: 'MA'
+const todos = [
+    {
+        id: 1,
+        text: 'Text 1',
+        iscompleted: true
+    },
+    {
+        id: 2,
+        text: 'Text 2',
+        iscompleted: true
+    },
+    {
+        id: 3,
+        text: 'Text 3',
+        iscompleted: false
     }
-}
+];
 
-person.email = 'john@gmail.com';
-
-
-const {firstName, lastName, address: { city }} = person;
-
-console.log(firstName);
-console.log(city);
-console.log(person);
+const todoJSON = JSON.stringify(todos);
+console.log(todoJSON);
